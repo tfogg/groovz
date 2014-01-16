2280abf915687bf8d26059cf2e5db3fa60e18bad
Thomas Fogg <foggts@g.cofc.edu>
2014-01-16 15:20:09 -0500

    created a rails app with status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Groovz::Application.config.secret_key_base = '003853e33115983aacc1801848fd67daf3d4d2eb15dad097948bd4c591e92592863ed2a6cc96bcdbb49ecf0d35fe290287229ca29c686fda7bd2ec55c0754b35'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Groovz::Application.config.session_store :cookie_store, key: '_groovz_session'
